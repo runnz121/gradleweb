cbdeda25b7b00c1317954f05b6db9aaefa110e4e
runnz <runnz121@gmail.com>
2021-03-17 23:21:31 +0900

    crate gradle project example

---
+++ src/main/java/start/gradleweb/GradlewebApplication.java
@@ -0,0 +1,13 @@
+package start.gradleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/start/gradleweb/GradlewebApplicationTests.java
@@ -0,0 +1,13 @@
+package start.gradleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
